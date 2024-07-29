74382e3f08ce304f970c4b553d31d59a31c87656
aseeralfaisal <aseeralfaisal@gmail.com>
2024-07-29 21:10:07 +0600

    added script

---
+++ init.zsh
@@ -0,0 +1,17 @@
+__initDotFiles() {
+  read -p "This will symblink all your dotfiles:(Y/n) " input
+  input=$(echo "$input" | tr '[:upper:]' '[:lower:]')
+
+  if [ "$input" = "y" ]; then
+    if sudo apt install stow -y; then 
+      stow .
+      echo "Dotfiles updated successfully! 🤟🎉"
+    else
+      echo "Error Occured during the process! 🎲"
+    fi
+  else
+    echo "Discarded Operation! 🎈"
+  fi
+}
+
+__initDotFiles
