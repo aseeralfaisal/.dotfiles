66182be2387baa1ae85d0ec52b438a6d18b1a9b8
A2Fyise <aseeralfaisal@gmail.com>
2024-12-19 14:54:56 -0500

    .zsh and tmux updated

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -24,14 +24,7 @@ return {
       },
     })
 
-    mason_lspconfig.setup({
-      -- list of servers for mason to install
-      ensure_installed = {
-        "tsserver",
-        "html",
-        "cssls",
-      },
-    })
+    mason_lspconfig.setup()
 
     mason_tool_installer.setup({
       ensure_installed = {
